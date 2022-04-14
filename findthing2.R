613eb847d936d08daf8c9fbac3ecffe31b9e5318
Koros <ivan.koros2@gmail.com>
2022-04-14 01:13:47 -0400

    second commit

--- findthing2.R
+++ findthing2.R
@@ -1,1 +1,19 @@
-thing2
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, openxlsx)
+
+
+
+findthing2 <- function(name, datasets) {
+  for (x in datasets) {
+    mdfeime <- read.xlsx(x)
+    set <- which(mdfeime == name, arr.ind = TRUE)
+    if (nrow(set) > 0) {
+      print(paste0(name, " is ", mdfeime[rownames(set), 10] , " in dataset ", x))
+    }
+  }
+} 
+
+findthing2(name = "Pafah1b3",
+           datasets = list("DP IR vs DP Sham Filtered.xlsx",
+                           "CD IR vs CD Sham Filtered.xlsx",
+                           "DP Sham vs CD Sham Filtered.xlsx"))
