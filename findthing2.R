9ed173d1d5f52c6f4c81d150d8bd43980fac1245
Koros <ivan.koros2@gmail.com>
2022-04-14 03:37:15 -0400

    added 3 decimal place log2f change

--- findthing2.R
+++ findthing2.R
@@ -6,18 +6,18 @@ pacman::p_load(tidyverse, openxlsx)
 findthing2 <- function(name, datasets) {
   for (x in datasets) {
     mdfeime <- read.xlsx(x)
-    set <- str_which(mdfeime$Symbol, name)
-    if ((str_detect(mdfeime$Symbol, name)))=T) {
-      print(paste0(name, " is ", mdfeime[rownames(set), 10] , " in dataset ", x))
+    set <- mdfeime[str_detect(mdfeime$Symbol, name), ]
+    if (nrow(set) > 0) {
+      print(paste0(mdfeime$Symbol[as.integer(row.names(set))],
+                   " is ",
+                   mdfeime$SaR[as.integer(row.names(set))] ,
+                   " in ",
+                   x))
     }
   }
-} 
+}
 
-findthing2(name = "Ltf",
+findthing2(name = "Bmp",
            datasets = list("DP IR vs DP Sham Filtered.xlsx",
                            "CD IR vs CD Sham Filtered.xlsx",
                            "DP Sham vs CD Sham Filtered.xlsx"))
-
-str_which(CDIRCDSHAM$Symbol, "Bmp")
-
-str_match_all(DPIRDPSHAM$Symbol, "Bmp")
